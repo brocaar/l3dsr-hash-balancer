3b1957d6fd1009f9bca45cf8c1bbd8e29c4504e1
Orne Brocaar <info@brocaar.com>
2015-04-22 14:18:25 -0300

    Fix import paths.

--- cmd/balancer/main.go
+++ cmd/balancer/main.go
@@ -12,9 +12,9 @@ import (
 	"sync"
 	"time"
 
-	"code.google.com/p/gopacket"
-	"code.google.com/p/gopacket/layers"
-	"code.google.com/p/gopacket/pcap"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
 )
 
 type tcpState uint8
